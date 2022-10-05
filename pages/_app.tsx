59813847f693570b9ca57b96dd51a748f2f7061f
Marquis Sampson <marquis4484@gmail.com>
2022-10-04 21:05:39 -0400

    _app change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,10 @@
-import {useEffect} from 'react';
-import {getCookieValue, setCookieValue} from '../utils/helpers'
+import React from 'react';
 
 import '../styles/globals.scss';
 import { Layout } from '../components';
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(()=>{
-    if (!getCookieValue('g-theme')) {
-      setCookieValue('g-theme', 'LIGHT', 2147483647, '/');
-    }
-  },[])
 
   return (
     <Layout>
